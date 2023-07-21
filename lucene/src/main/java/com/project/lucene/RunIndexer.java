861e6657610e0c6f9a81999606543d04b7be302b
eybirdy <ahadiyat.abadi@gmail.com>
2023-07-22 00:36:32 +0800

    Added DocumentItem DTO

--- lucene/src/main/java/com/project/lucene/RunIndexer.java
+++ lucene/src/main/java/com/project/lucene/RunIndexer.java
@@ -1,10 +1,18 @@
 package com.project.lucene;
 
+import java.util.List;
+
+import org.apache.lucene.document.Document;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.project.lucene.model.DocumentItem;
 import com.project.lucene.util.Indexing;
 import com.project.lucene.util.LuceneFileSearch;
+import com.project.lucene.util.YourClassName;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 
 import lombok.AllArgsConstructor;
 
@@ -22,7 +30,26 @@ public class RunIndexer implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+
+		
+//		indexing.indexDocument("This is a title", "Test");
+//		indexing.indexDocument("This is a test", "Title");
+		List<Document> testList = indexing.searchIndex("title", "test");
+		
+		System.out.println(testList);
+		
 	}
+	
+	public List<DocumentItem> readJsonFileAndParse(String filePath) throws IOException {
+        // Create an instance of ObjectMapper from Jackson
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Read the JSON file and parse it into a List of ItemDTO objects
+        List<DocumentItem> items = objectMapper.readValue(new File(filePath),
+                                                     objectMapper.getTypeFactory().constructCollectionType(List.class, DocumentItem.class));
+
+        return items;
+    }
 
 
 	
---
+++ lucene/src/main/java/com/project/lucene/model/DocumentItem.java
@@ -0,0 +1,16 @@
+package com.project.lucene.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DocumentItem {
+    public String title;
+    public String content;
+    public String url;
+
+}
