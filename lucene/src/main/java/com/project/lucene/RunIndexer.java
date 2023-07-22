f7562d6615289d726d791bad80a0ae6a2b13afaa
AkmalMuhd <AkmalMuhd@hotmail.com>
2023-07-23 00:11:41 +0800

    update

--- lucene/src/main/java/com/project/lucene/RunIndexer.java
+++ lucene/src/main/java/com/project/lucene/RunIndexer.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.project.lucene.model.DocumentItem;
+import com.project.lucene.model.ResultItem;
 import com.project.lucene.util.Indexing;
 import com.project.lucene.util.LuceneFileSearch;
 
@@ -30,28 +31,27 @@ public class RunIndexer implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		//String filePath = "../data_directory/data.json";
-
+		String filePath = "../data_directory/data.json";
 		
-//		indexing.indexDocument("This is a title", "Test");
-//		indexing.indexDocument("This is a test", "Title");
-//		List<Document> testList = indexing.searchIndex("title", "test");
-//		
-//		System.out.println(testList);
+		indexJson(filePath);
 		
-		// Read in JSON file as a list of documents
-//		List<DocumentItem> documentsList = readJsonFileAndParse(filePath);
-//		
-//		// Iterate through list of documents and index 
-//		for(DocumentItem item: documentsList) {
-//			System.out.println(item.title);
-//			indexing.indexDocument(item.title, item.content ,item.url);
-//		}
+		List<Document> testList = indexing.searchIndex("title", "information");
+		System.out.println("===================");
 		
-		//List<Document> testList = indexing.searchIndex("title", "test");
+		for(Document item: testList) {
+			System.out.println(item.get("title"));
+		}
+		System.out.println("Multi here ===================");
+		
+		ResultItem testList2 = indexing.querySearchIndex("information", 1, 20);
+		
+		for(DocumentItem item: testList2.getDocuments()) {
+			System.out.println(item.title);
+		}
 		
 	}
 	
+	// Method to read in JSON
 	public List<DocumentItem> readJsonFileAndParse(String filePath) throws IOException {
         // Create an instance of ObjectMapper from Jackson
         ObjectMapper objectMapper = new ObjectMapper();
@@ -62,6 +62,55 @@ public class RunIndexer implements CommandLineRunner{
 
         return items;
     }
+	
+	// Function to read in JSON and index documents from the JSON
+	public void indexJson(String filePath) throws IOException {
+		// Read in JSON file as a list of documents
+		List<DocumentItem> documentsList = readJsonFileAndParse(filePath);
+		System.out.println("Indexing started");
+		
+		int count = 0;
+		
+		for(DocumentItem item: documentsList) {
+			indexing.indexDocument(item.title.toString(), item.content ,item.url);
+			count++;
+			System.out.println(Integer.toString(count) + ". Indexed: " + item.title.toString());
+		}
+		
+		
+//		for(DocumentItem item: documentsList) {
+//			System.out.println("From JSON: " + item.url);
+//			List<Document> docList = indexing.searchIndex("url", item.url);
+//		
+//			if(docList == null) {
+//				System.out.println("docList is null");
+//				indexing.indexDocument(item.title, item.content ,item.url);
+//				count++;
+//				System.out.println(Integer.toString(count) + ". Indexed: " + item.url);
+//				
+//				
+//			}else {
+//				System.out.println("docList is NOT null");
+//				for (Document doc: docList) {
+//					System.out.println("From query: " + doc.get("title"));
+//					if(doc.get("url").equals(item.url) ) {
+//						System.out.println("Document has already been indexed");
+//						break;
+//						
+//						
+//					}else {
+//						indexing.indexDocument(item.title, item.content ,item.url);
+//						count++;
+//						System.out.println(Integer.toString(count) + ". Indexed: " + item.url);
+//						break;
+//						
+//					}
+//				}
+//			}
+//		}
+		
+		System.out.println("Indexed " + count + " documents");
+	}
 
 
 	
--- lucene/src/main/java/com/project/lucene/util/Indexing.java
+++ lucene/src/main/java/com/project/lucene/util/Indexing.java
@@ -16,6 +16,7 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
@@ -23,6 +24,7 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.Sort;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.util.BytesRef;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -73,6 +75,9 @@ public class Indexing {
 
             IndexReader indexReader = DirectoryReader.open(memoryIndex);
             IndexSearcher searcher = new IndexSearcher(indexReader);
+            
+
+            
             TopDocs topDocs = searcher.search(query, 10);
             List<Document> documents = new ArrayList<>();
             for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
@@ -145,6 +150,76 @@ public class Indexing {
         }
         return documentItems;
     }
+    
+    // Method to use multiple fields in one query
+    public ResultItem querySearchIndex(String queryString, int currentPageNum, int pageSize) {
+        try {
+        	
+        	String[] searchFields = new String[]{"title", "body"};
+        	MultiFieldQueryParser queryParser = new MultiFieldQueryParser(searchFields, analyzer);
+            Query query = queryParser.parse(queryString);
+            
+            System.out.println(query);
+            
+            // Calculate the starting index of the documents for the given page
+            int start = (currentPageNum - 1) * pageSize;
+
+            IndexReader indexReader = DirectoryReader.open(memoryIndex);
+            IndexSearcher searcher = new IndexSearcher(indexReader);
+//            TopDocs topDocs = searcher.search(query, 10);
+            
+//            Apply title boosting here
+//            float titleBoost = 2.0f; // Adjust the boosting factor based on your requirement
+//            query = applyTitleBoost(query, titleBoost);
+            
+            float k1 = 1.5f;
+            float b = 0.75f;
+            
+            searcher.setSimilarity(new BM25Similarity(k1, b));
+            
+//           Perform the search and retrieve documents from the specified page
+            TopDocs topDocs = searcher.search(query, start + pageSize);
+            ScoreDoc[] hits = topDocs.scoreDocs;
+//            List<Document> documents = new ArrayList<>();
+//            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+//                documents.add(searcher.doc(scoreDoc.doc));
+//            }
+            
+            List<Document> documents = new ArrayList<>();
+            for (int i = start; i < Math.min(hits.length, start + pageSize); i++) {
+                Document doc = searcher.doc(hits[i].doc);
+                documents.add(doc);
+            }
+
+            // Convert TopDocs.totalHits to int
+            int totalNumOfResults = Math.toIntExact(topDocs.totalHits.value);
+            
+            // Calculate the number of pages
+            int totalNumOfPages = (int) Math.ceil((double) totalNumOfResults / pageSize);
+            
+            return new ResultItem(pageSize, currentPageNum, totalNumOfPages, totalNumOfResults, parseDocumentList(documents));
+
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }   
+    
+//    private Query applyTitleBoost(Query query, float titleBoost) {
+//        // Clone the original query to avoid modifying it directly
+//        Query clonedQuery = query.clone();
+//
+//        // Create a BooleanQuery with the original query as "SHOULD" clause and the title query as "MUST" clause
+//        BooleanQuery.Builder booleanQuery = new BooleanQuery.Builder();
+//        booleanQuery.add(clonedQuery, BooleanClause.Occur.SHOULD); // Original query as SHOULD
+//        booleanQuery.add(new BoostQuery(clonedQuery, titleBoost), BooleanClause.Occur.MUST); // Title query as MUST with boost
+//
+//        return booleanQuery.build();
+
+    
+    
 
     public void deleteDocument(Term term) {
         try {
